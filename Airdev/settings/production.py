dcdb121c2c13cfdfdb4db377337146837ebda292
JIT <inayat@algoson.com>
2022-05-02 12:25:28 +0530

    production added

--- Airdev/settings/production.py
+++ Airdev/settings/production.py
@@ -4,7 +4,7 @@ from django.core.management.utils import get_random_secret_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 SECRET_KEY = get_random_secret_key()
-ALLOWED_HOSTS = ['206.189.134.136', 'localhost']
+ALLOWED_HOSTS = ['206.189.134.136']
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
